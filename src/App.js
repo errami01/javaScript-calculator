b622b34386d0256b306d8b10c39fc42ed3b3b5d9
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-04-03 16:12:17 +0000

    Create data file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
+import data from './data';
 function App() {
-const keys =['AC', '/','X', 7, 8, 9, '-', 4, 5, 6, '+', 1,2,3, '=', 0,'.']
+// const keys =['AC', '/','X', 7, 8, 9, '-', 4, 5, 6, '+', 1,2,3, '=', 0,'.']
 
-  const calcKeys = keys.map((key)=>{
-    return  (<div key={key}>{key}</div>)
+
+  const calcKeys = data.map(({id, value, role})=>{ 
+    return  (<div id={id} className={`${id} ${role}`} key={id} >{value}</div>)
   })
   return (
     
     <div className="App">
-      <div className='keyboard-grid'>
-        {calcKeys}
+      <div className='calculator'>
+        <div className='keyboard-grid'>
+          {calcKeys}
+        </div>
       </div>
-      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,79 @@
+const data =[
+    {
+    id: 'clear',
+    value: 'AC',
+    role: 'clear'
+  },
+  {
+    id: 'divide',
+    value: '/',
+    role: 'operator'
+  },
+  {
+    id: 'multiply',
+    value: 'X',
+    role: 'operator'
+  },
+  {
+    id: 'seven',
+    value: 7,
+    role: "number"
+  },
+  {
+    id: 'eight',
+    value: 8,
+    role: "number"
+  },
+  {
+    id: 'nine',
+    value: 9,
+    role: "number"
+  },
+  {
+    id: 'subtract',
+    value: '-',
+    role: 'operator'
+  },
+  {
+    id: 'four',
+    value: 4,
+    role: "number"
+  },{
+    id: 'five',
+    value: 5,
+    role: "number"
+  },
+  {
+    id: 'six',
+    value: 6,
+    role: "number"
+  },{
+    id: 'add',
+    value: '+',
+    role: 'operator'
+  },{
+    id: 'one',
+    value: 1,
+    role: "number"
+  },{
+    id: 'two',
+    value: 2,
+    role: "number"
+  },{
+    id: 'three',
+    value: 3,
+    role: "number"
+  },{
+    id: 'equals',
+    value: '='
+  },{
+    id: 'zero',
+    value: 0,
+    role: "number"
+  },{
+    id: 'decimal',
+    value: '.',
+    role: "number"
+  }
+]
+  export default data
