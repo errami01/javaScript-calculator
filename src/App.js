2091b7df57db848d00f9686b6dfeaf973809d7a7
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-04-03 03:58:02 +0000

    Create keys array and wrap them in dev using map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
 function App() {
+const keys =['AC', '/','X', 7, 8, 9, '-', 4, 5, 6, '+', 1,2,3, '=', 0,'.']
+
+  const calcKeys = keys.map((key)=>{
+    return  (<div key={key}>{key}</div>)
+  })
   return (
+    
     <div className="App">
-      <h1>Hello</h1>
+      <div className='keyboard-grid'>
+        {calcKeys}
+      </div>
+      
     </div>
   );
 }
